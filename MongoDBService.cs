94765bcdfad31d3699e0b9e8857fd59d8c498cc1
Amna gul <amnagul048@gmail.com>
2026-06-07 00:38:38 +0500

    Add files via upload

--- MongoDBService.cs
+++ MongoDBService.cs
@@ -1,3 +1,19 @@
-﻿internal class MongoDBService
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using real_time_chat.Models;
+
+namespace real_time_chat.Services
 {
+    public class MongoDbService
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbService(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetConnectionString("MongoDbConnection"));
+            _database = client.GetDatabase("ChatAppDB");
+        }
+
+        public IMongoCollection<User> Users => _database.GetCollection<User>("Users");
+    }
 }
